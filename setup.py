f6c9118c0dd7a05205760f04d5cb1d287ad9b072
Raf Guns <raf.guns@uantwerpen.be>
2023-09-20 12:35:31 +0200

    Support Python versions 3.9 to 3.11

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
-long_description = open('README.md', encoding='utf-8').read()
+long_description = open("README.md", encoding="utf-8").read()
 
 setup(
-    name='gefura',
-    version='0.1',
-    url='http://github.com/rafguns/gefura/',
-    license='New BSD License',
-    author='Raf Guns',
-    tests_require=['pytest', 'pytest-cov'],
+    name="gefura",
+    version="0.1",
+    url="http://github.com/rafguns/gefura/",
+    license="New BSD License",
+    author="Raf Guns",
+    tests_require=["pytest", "pytest-cov"],
     install_requires=[
-        'networkx>=2.8',
+        "networkx>=3.0",
     ],
-    author_email='raf.guns@uantwerpen.be',
-    description='Discover \'bridges\' between node groups in a network',
+    author_email="raf.guns@uantwerpen.be",
+    description="Discover 'bridges' between node groups in a network",
     long_description=long_description,
-    py_modules=['gefura'],
-    platforms='any',
+    py_modules=["gefura"],
+    platforms="any",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development',
-        'Topic :: Scientific/Engineering',
-        'Development Status :: 4 - Beta',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-    ]
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
 )
