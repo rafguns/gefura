226d86c254bd0c739c0431cb5a8fffb496a4e241
Raf Guns <raf.guns@uantwerpen.be>
2024-06-25 09:01:03 +0200

    Implement option to limit maximum path length (#34)
    Implement max_path_length for global_gefura (unweighted) and local_gefura (unweighted)
    
    Fixes #30

--- gefura.py
+++ gefura.py
@@ -10,14 +10,13 @@ undirected, as well as weighted and unweighted networks are supported.
 Overlapping groups are currently only supported for global gefura.
 
 """
-from collections import defaultdict
+from collections import defaultdict, deque
 from itertools import combinations
 from typing import Iterable, Literal, Optional, Union
 
 import networkx as nx
 from networkx.algorithms.centrality.betweenness import (
     _single_source_dijkstra_path_basic,
-    _single_source_shortest_path_basic,
 )
 
 __version__ = "0.2"
@@ -35,12 +34,44 @@ def _groups_per_node(groups: Iterable[set[Node]]) -> dict[Node, set[int]]:
     return d
 
 
+def _single_source_shortest_path_basic(G, s, max_path_length=None):
+    """Adapted from networkx.algorithms.centrality.betweenness
+
+    Includes optional maximum path length
+
+    """
+    S = []
+    P = {}
+    for v in G:
+        P[v] = []
+    sigma = dict.fromkeys(G, 0.0)
+    dist = {}
+    sigma[s] = 1.0
+    dist[s] = 0
+    Q = deque([s])
+    while Q:  # use BFS to find shortest paths
+        v = Q.popleft()
+        S.append(v)
+        distv = dist[v]
+        sigmav = sigma[v]
+        for w in G[v]:
+            if w not in dist:
+                dist[w] = distv + 1
+                if max_path_length is None or dist[w] <= max_path_length:
+                    Q.append(w)
+            if dist[w] == distv + 1:  # this is a shortest path, count paths
+                sigma[w] += sigmav
+                P[w].append(v)  # predecessors
+    return S, P, sigma, dist
+
+
 def global_gefura(
     G: nx.Graph,
     groups: Iterable[set[Node]],
     *,
     weight: Optional[str] = None,
     normalized: bool = True,
+    max_path_length: Optional[int] = None,
 ) -> dict[Node, float]:
     """Determine global gefura measure of each node
 
@@ -62,6 +93,9 @@ def global_gefura(
     normalized : True|False
         Whether or not to normalize the output to [0, 1].
 
+    max_path_length : int or None
+        Maximum number of edges to traverse (unweighted only)
+
     Examples
     --------
     >>> import networkx as nx
@@ -79,7 +113,7 @@ def global_gefura(
 
     for s in G:
         if weight is None:
-            S, P, sigma, _ = _single_source_shortest_path_basic(G, s)
+            S, P, sigma, _ = _single_source_shortest_path_basic(G, s, max_path_length)
         else:
             S, P, sigma, _ = _single_source_dijkstra_path_basic(G, s, weight)
 
@@ -108,6 +142,7 @@ def _local_gefura(
     *,
     weight: Optional[str] = None,
     normalized: bool = True,
+    max_path_length: Optional[int] = None,
 ) -> dict[Node, float]:
     gamma = dict.fromkeys(G, 0)
     group_of = _groups_per_node(groups)
@@ -117,7 +152,9 @@ def _local_gefura(
 
     for s in G:
         if weight is None:
-            S, P, sigma, _ = _single_source_shortest_path_basic(G, s)
+            S, P, sigma, _ = _single_source_shortest_path_basic(
+                G, s, max_path_length=max_path_length
+            )
         else:
             S, P, sigma, _ = _single_source_dijkstra_path_basic(G, s, weight)
 
@@ -147,6 +184,7 @@ def local_gefura(
     weight: Optional[str] = None,
     normalized: bool = True,
     direction: Literal["in", "out", "all"] = "out",
+    max_path_length: Optional[int] = None,
 ) -> dict[Node, float]:
     """Determine local gefura measure of each node
 
@@ -175,6 +213,9 @@ def local_gefura(
         - 'in' is based on all paths from elsewhere to the own group
         - 'out' is based on all paths from the own group to elsewhere
 
+    max_path_length : int or None
+        Maximum number of edges to traverse (unweighted only)
+
     Examples
     --------
     >>> import networkx as nx
@@ -184,8 +225,13 @@ def local_gefura(
     {0: 0.0, 1: 0, 2: 0.6666666666666666, 3: 1.0, 4: 0.0}
 
     """
+    kwargs = {
+        "weight": weight,
+        "normalized": normalized,
+        "max_path_length": max_path_length,
+    }
     if not G.is_directed() or direction == "out":
-        return _local_gefura(G, groups, weight=weight, normalized=normalized)
+        return _local_gefura(G, groups, **kwargs)
 
     if direction not in ("in", "all"):
         msg = "Direction should be either 'in', 'out' or 'all'."
@@ -193,14 +239,12 @@ def local_gefura(
 
     # The algorithm follows the 'out' direction in directed graphs.
     # For 'in', we reverse the direction prior to applying the algorithm.
-    gamma_in = _local_gefura(
-        G.reverse(copy=False), groups, weight=weight, normalized=normalized
-    )
+    gamma_in = _local_gefura(G.reverse(copy=False), groups, **kwargs)
     if direction == "in":
         return gamma_in
 
     # 'all' is the sum of 'in' and 'out'
-    gamma_out = _local_gefura(G, groups, weight=weight, normalized=normalized)
+    gamma_out = _local_gefura(G, groups, **kwargs)
     norm = 2 if normalized else 1  # Count both A -> gamma and gamma -> A
 
     return {k: (gamma_in[k] + gamma_out[k]) / norm for k in gamma_in}
